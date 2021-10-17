8152f134186a7d056d675cf06ddb9ffdd07e17e9
Youssef Restom <youssefrestom@gmail.com>
2021-10-17 16:35:08 +0200

    feat: init Doctypes

---
+++ bulkwebhook/bulk_webhook/doctype/bulk_webhook/bulk_webhook.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Aakvatech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bulk Webhook', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ bulkwebhook/bulk_webhook/doctype/bulk_webhook_settings/bulk_webhook_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Aakvatech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bulk Webhook Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
