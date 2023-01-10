469dddb96158eac7bf3976fcf29b36dbcebff600
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2023-01-10 12:27:49 +0300

    KEN-541 fix: referenced the right method

--- bulkwebhook/api/api.py
+++ bulkwebhook/api/api.py
@@ -24,12 +24,12 @@ def resend_kafkahook_for_docs(args):
         frappe.throw("Doc List is required")
     kafkahook = {}
     kafkahook["name"] = kafkahook_name
-    from bulkwebhook.bulk_webhook.doctype.kafka_hook.kafka_hook import enqueue_webhook
+    from bulkwebhook.bulk_webhook.doctype.kafka_hook.kafka_hook import enqueue_kafka_hook
 
     for doc_name in doc_list:
         doc = frappe.get_doc(doctype_name, doc_name)
         enqueue(
-            method=enqueue_webhook,
+            method=enqueue_kafka_hook,
             queue="long",
             timeout=10000,
             is_async=True,
